48ef3d192f8aef0c052756bba97c8f175697cb08
nuwanVDV <166138247+nuwanVDV@users.noreply.github.com>
2024-08-08 21:38:04 +0530

    Create v1.js

---
+++ v1.js
@@ -0,0 +1,50 @@
+// Time line code
+let dentistNameDan = document.querySelector(".dentist-name-dan");
+let dentistImgDan = document.querySelector(".dentist-image-dan");
+
+dentistNameDan.addEventListener("mouseover", () => {
+    dentistImgDan.src = "https://wordpress-183497-4713410.cloudwaysapps.com/wp-content/uploads/2024/07/Dan-headshot.jpg";
+});
+dentistImgDan.addEventListener("mouseover", () => {
+    dentistImgDan.src = "https://wordpress-183497-4713410.cloudwaysapps.com/wp-content/uploads/2024/07/Dan-headshot.jpg";
+});
+dentistNameDan.addEventListener("mouseout", () => {
+    dentistImgDan.src = "https://wordpress-183497-4713410.cloudwaysapps.com/wp-content/uploads/2024/07/Dan-headshot-with-mask.jpeg";
+});
+dentistImgDan.addEventListener("mouseout", () => {
+	dentistImgDan.src = "https://wordpress-183497-4713410.cloudwaysapps.com/wp-content/uploads/2024/07/Dan-headshot-with-mask.jpeg";
+});
+
+let dentistNameAlina = document.querySelector(".dentist-name-alina");
+let dentistImgAlina = document.querySelector(".dentist-image-alina");
+
+dentistNameAlina.addEventListener("mouseover", () => {
+    dentistImgAlina.src = "https://wordpress-183497-4713410.cloudwaysapps.com/wp-content/uploads/2024/07/Alina-headshot.jpg";
+});
+dentistNameAlina.addEventListener("mouseout", () => {
+    dentistImgAlina.src = "https://wordpress-183497-4713410.cloudwaysapps.com/wp-content/uploads/2024/07/Alina-headshot-with-mask.jpeg";
+});
+dentistImgAlina.addEventListener("mouseover", () => {
+    dentistImgAlina.src = "https://wordpress-183497-4713410.cloudwaysapps.com/wp-content/uploads/2024/07/Alina-headshot.jpg";
+});
+dentistImgAlina.addEventListener("mouseout", () => {
+    dentistImgAlina.src = "https://wordpress-183497-4713410.cloudwaysapps.com/wp-content/uploads/2024/07/Alina-headshot-with-mask.jpeg";
+});
+
+let dentistNameMike = document.querySelector(".dentist-name-mike");
+let dentistImgMike = document.querySelector(".dentist-image-mike");
+
+
+dentistNameMike.addEventListener("mouseover", () => {
+    dentistImgMike.src = "https://wordpress-183497-4713410.cloudwaysapps.com/wp-content/uploads/2024/07/Mike-headshot.jpg";
+});
+dentistNameMike.addEventListener("mouseout", () => {
+    dentistImgMike.src = "https://wordpress-183497-4713410.cloudwaysapps.com/wp-content/uploads/2024/07/Mike-headshot-with-mask.jpeg";
+});
+dentistImgMike.addEventListener("mouseover", () => {
+    dentistImgMike.src = "https://wordpress-183497-4713410.cloudwaysapps.com/wp-content/uploads/2024/07/Mike-headshot.jpg";
+});
+dentistImgMike.addEventListener("mouseout", () => {
+    dentistImgMike.src = "https://wordpress-183497-4713410.cloudwaysapps.com/wp-content/uploads/2024/07/Mike-headshot-with-mask.jpeg";
+});
+
